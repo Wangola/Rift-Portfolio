db0239425900fb0cec96d97aadcdd25b6eba1ad7
Wangola <Wangola99@gmail.com>
2023-07-08 16:19:22 -0400

    Completed player movement with smooth transitions. Need to apply physics now.

--- src/components/TestingMov.jsx
+++ src/components/TestingMov.jsx
@@ -6,10 +6,52 @@ import * as THREE from "three";
 // Custom import
 import Character from "./Character";
 
+/**
+ * Calculate the target rotation for the body based on the camera and character position.
+ *
+ * @param {Vector3} cameraPosition - Position of the camera
+ * @param {Vector3} characterPosition - Position of the character
+ * @param {number} additionalRotation - Additional rotation for selected direction
+ * @returns {number} Target rotation
+ */
+function calculateTargetRotation(
+  cameraPosition,
+  characterPosition,
+  additionalRotation
+) {
+  return (
+    Math.atan2(
+      cameraPosition.x - characterPosition.x,
+      cameraPosition.z - characterPosition.z
+    ) + additionalRotation
+  );
+}
+
+/**
+ * Smoothly interpolate the current body rotation towards the target rotation.
+ *
+ * @param {number} currentRotation - Current body rotation
+ * @param {number} targetRotation - Target body rotation
+ * @param {number} rotationSpeed - Speed of rotation interpolation
+ * @param {number} delta - Time since last frame
+ * @returns {number} Smoothed rotation value
+ */
+function smoothRotation(currentRotation, targetRotation, rotationSpeed, delta) {
+  // Calculate the rotation difference
+  let rotationDifference = targetRotation - currentRotation;
+
+  // Adjust the rotation difference to choose the shorter path
+  if (rotationDifference > Math.PI) {
+    rotationDifference -= 2 * Math.PI;
+  } else if (rotationDifference < -Math.PI) {
+    rotationDifference += 2 * Math.PI;
+  }
+
+  return currentRotation + rotationDifference * rotationSpeed * delta;
+}
+
 export default function TestingMov() {
-  /**
-   * Ref
-   */
+  // Ref
   const body = useRef();
 
   /**
@@ -21,9 +63,6 @@ export default function TestingMov() {
    * subscribeKeys (gets key changes) getKeys (gets key states)
    */
   const [subscribeKeys, getKeys] = useKeyboardControls();
-
-  // Velocity handler
-  const [velocity, setVelocity] = useState({ x: 0, y: 0, z: 0 });
 
   /**
    * Used for lerping position and target
@@ -37,6 +76,15 @@ export default function TestingMov() {
   const [rotationAngle, setRotationAngle] = useState(Math.PI / 4);
   const cameraDistance = 5.5; // Adjust this value to control the camera's distance from the object
 
+  /**
+   * Handle Character Rotations Speeds
+   */
+  // Body rotation speed
+  const rotationSpeed = 6;
+
+  // Camera rotation speed
+  const cameraTurnSpeed = 0.015;
+
   useFrame((state, delta) => {
     /**
      * Controls
@@ -46,7 +94,7 @@ export default function TestingMov() {
     /**
      * Speed
      */
-    const speed = shift ? 7 : 5; // Adjust the speed based on whether shift is pressed or not
+    const speed = shift ? 7.5 : 4.5; // Adjust the speed based on whether shift is pressed or not
     let newVelocity = { x: 0, y: 0, z: 0 };
 
     // Movement Direction represents the direction in which the player should move forward or backward based on the camera's rotation
@@ -57,121 +105,301 @@ export default function TestingMov() {
     );
 
     /**
-     * Movement
+     * Movement direction
      */
     switch (true) {
-      case forward && !leftward && !rightward:
-        // Handle forward movement (F)
-        newVelocity.x = movementDirection.x * -speed * delta;
-        newVelocity.z = movementDirection.z * -speed * delta;
+      // Handle edge case (where both are pressed stops movement)
+      case forward && backward:
+        {
+          // Handle simultaneous press of "left" and "right" buttons
+          newVelocity.x = 0; // No movement in the x-axis
+          newVelocity.z = 0; // No movement in the z-axis
 
-        body.current.rotation.y =
-          Math.atan2(
-            state.camera.position.x - body.current.position.x,
-            state.camera.position.z - body.current.position.z
-          ) +
-          (7 * Math.PI) / 4;
-
-        setAnimationName(shift ? "Run" : "Walk");
+          // Set animation name to "Idle"
+          setAnimationName("Idle");
+        }
         break;
 
-      case backward && !leftward && !rightward:
-        // Handle backward movement (B)
-        newVelocity.x = movementDirection.x * speed * delta;
-        newVelocity.z = movementDirection.z * speed * delta;
+      case (forward && !leftward && !rightward) ||
+        (forward && leftward && rightward):
+        {
+          // Handle forward movement (F)
+          newVelocity.x = movementDirection.x * -speed * delta;
+          newVelocity.z = movementDirection.z * -speed * delta;
 
-        body.current.rotation.y =
-          Math.atan2(
-            state.camera.position.x - body.current.position.x,
-            state.camera.position.z - body.current.position.z
-          ) +
-          (3 * Math.PI) / 4; // Add a rotation of 3π/4 (135 degrees)
+          // Calc target rotation for forward
+          const targetRotation = calculateTargetRotation(
+            state.camera.position,
+            body.current.position,
+            (7 * Math.PI) / 4
+          );
 
-        setAnimationName(shift ? "Run" : "Walk");
+          // Interpolate current with target
+          const smoothedRotation = smoothRotation(
+            body.current.rotation.y,
+            targetRotation,
+            rotationSpeed,
+            delta
+          );
+
+          // Keep the rotation within the range of 0 and 2π
+          body.current.rotation.y =
+            (2 * Math.PI + smoothedRotation) % (2 * Math.PI);
+
+          // Set animation name based on movement speed
+          setAnimationName(shift ? "Run" : "Walk");
+        }
+        break;
+
+      case (backward && !leftward && !rightward) ||
+        (backward && leftward && rightward):
+        {
+          // Handle backward movement (B)
+          newVelocity.x = movementDirection.x * speed * delta;
+          newVelocity.z = movementDirection.z * speed * delta;
+
+          // Calc target rotation for backward
+          const targetRotation = calculateTargetRotation(
+            state.camera.position,
+            body.current.position,
+            (3 * Math.PI) / 4
+          );
+
+          // Interpolate current with target
+          const smoothedRotation = smoothRotation(
+            body.current.rotation.y,
+            targetRotation,
+            rotationSpeed,
+            delta
+          );
+
+          // Keep the rotation within the range of 0 and 2π
+          body.current.rotation.y =
+            (2 * Math.PI + smoothedRotation) % (2 * Math.PI);
+
+          // Set animation name based on movement speed
+          setAnimationName(shift ? "Run" : "Walk");
+        }
+
+        break;
+
+      // Handle edge case (where both are pressed stops movement)
+      case leftward && rightward:
+        {
+          // Handle simultaneous press of "left" and "right" buttons
+          newVelocity.x = 0; // No movement in the x-axis
+          newVelocity.z = 0; // No movement in the z-axis
+
+          // Set animation name to "Idle"
+          setAnimationName("Idle");
+        }
         break;
 
       case leftward && !forward && !backward:
-        // Handle leftward movement (L)
-        newVelocity.x = -movementDirection.z * speed * delta;
-        newVelocity.z = movementDirection.x * speed * delta;
-        body.current.rotation.y =
-          Math.atan2(
-            state.camera.position.x - body.current.position.x,
-            state.camera.position.z - body.current.position.z
-          ) +
-          Math.PI / 4;
+        {
+          // Handle leftward movement (L)
+          newVelocity.x = -movementDirection.z * speed * delta;
+          newVelocity.z = movementDirection.x * speed * delta;
 
-        setAnimationName(shift ? "Run" : "Walk");
+          // Calc target rotation for leftward
+          const targetRotation = calculateTargetRotation(
+            state.camera.position,
+            body.current.position,
+            Math.PI / 4
+          );
+
+          // Interpolate current with target
+          const smoothedRotation = smoothRotation(
+            body.current.rotation.y,
+            targetRotation,
+            rotationSpeed,
+            delta
+          );
+
+          // Keep the rotation within the range of 0 and 2π
+          body.current.rotation.y =
+            (2 * Math.PI + smoothedRotation) % (2 * Math.PI);
+
+          // Set animation name based on movement speed
+          setAnimationName(shift ? "Run" : "Walk");
+
+          // Apply left camera rotation using cameraTurnSpeed [+ for left]
+          setRotationAngle(rotationAngle + cameraTurnSpeed);
+        }
         break;
 
       case rightward && !forward && !backward:
-        // Handle rightward movement (R)
-        newVelocity.x = movementDirection.z * speed * delta;
-        newVelocity.z = -movementDirection.x * speed * delta;
-        body.current.rotation.y =
-          Math.atan2(
-            state.camera.position.x - body.current.position.x,
-            state.camera.position.z - body.current.position.z
-          ) +
-          (5 * Math.PI) / 4; // Add a rotation of 5π/4 (225 degrees)
-        setAnimationName(shift ? "Run" : "Walk");
+        {
+          // Handle rightward movement (R)
+          newVelocity.x = movementDirection.z * speed * delta;
+          newVelocity.z = -movementDirection.x * speed * delta;
+
+          // Calc target rotation for rightward
+          const targetRotation = calculateTargetRotation(
+            state.camera.position,
+            body.current.position,
+            (5 * Math.PI) / 4
+          );
+
+          // Interpolate current with target
+          const smoothedRotation = smoothRotation(
+            body.current.rotation.y,
+            targetRotation,
+            rotationSpeed,
+            delta
+          );
+
+          // Keep the rotation within the range of 0 and 2π
+          body.current.rotation.y =
+            (2 * Math.PI + smoothedRotation) % (2 * Math.PI);
+
+          // Set animation name based on movement speed
+          setAnimationName(shift ? "Run" : "Walk");
+
+          // Apply right camera rotation using cameraTurnSpeed [- for right]
+          setRotationAngle(rotationAngle - cameraTurnSpeed);
+        }
         break;
 
       case forward && leftward:
-        // Handle simultaneous press of "left" and "forward" buttons (FL)
-        newVelocity.x =
-          (-movementDirection.x - movementDirection.z) * speed * delta;
-        newVelocity.z =
-          (-movementDirection.z + movementDirection.x) * speed * delta;
-        body.current.rotation.y = Math.atan2(
-          state.camera.position.x - body.current.position.x,
-          state.camera.position.z - body.current.position.z
-        );
-        setAnimationName(shift ? "Run" : "Walk");
+        {
+          // Handle simultaneous press of "left" and "forward" buttons (FL)
+          newVelocity.x =
+            (-movementDirection.x - movementDirection.z) * speed * delta;
+          newVelocity.z =
+            (-movementDirection.z + movementDirection.x) * speed * delta;
+
+          // Calc target rotation for forward && leftward
+          const targetRotation = calculateTargetRotation(
+            state.camera.position,
+            body.current.position,
+            0
+          );
+
+          // Interpolate current with target
+          const smoothedRotation = smoothRotation(
+            body.current.rotation.y,
+            targetRotation,
+            rotationSpeed,
+            delta
+          );
+
+          // Keep the rotation within the range of 0 and 2π
+          body.current.rotation.y =
+            (2 * Math.PI + smoothedRotation) % (2 * Math.PI);
+
+          // Set animation name based on movement speed
+          setAnimationName(shift ? "Run" : "Walk");
+
+          // Apply left camera rotation using cameraTurnSpeed [+ for left]
+          setRotationAngle(rotationAngle + cameraTurnSpeed);
+        }
         break;
 
       case forward && rightward:
-        // Handle simultaneous press of "left" and "forward" buttons (FR)
-        newVelocity.x =
-          (movementDirection.x - movementDirection.z) * -speed * delta;
-        newVelocity.z =
-          (movementDirection.z + movementDirection.x) * -speed * delta;
-        body.current.rotation.y =
-          Math.atan2(
-            state.camera.position.x - body.current.position.x,
-            state.camera.position.z - body.current.position.z
-          ) +
-          (3 * Math.PI) / 2;
-        setAnimationName(shift ? "Run" : "Walk");
+        {
+          // Handle simultaneous press of "left" and "forward" buttons (FR)
+          newVelocity.x =
+            (movementDirection.x - movementDirection.z) * -speed * delta;
+          newVelocity.z =
+            (movementDirection.z + movementDirection.x) * -speed * delta;
+
+          // Calc target rotation for forward && rightward
+          const targetRotation = calculateTargetRotation(
+            state.camera.position,
+            body.current.position,
+            (3 * Math.PI) / 2
+          );
+
+          // Interpolate current with target
+          const smoothedRotation = smoothRotation(
+            body.current.rotation.y,
+            targetRotation,
+            rotationSpeed,
+            delta
+          );
+
+          // Keep the rotation within the range of 0 and 2π
+          body.current.rotation.y =
+            (2 * Math.PI + smoothedRotation) % (2 * Math.PI);
+
+          // Set animation name based on movement speed
+          setAnimationName(shift ? "Run" : "Walk");
+
+          // Apply right camera rotation using cameraTurnSpeed [- for right]
+          setRotationAngle(rotationAngle - cameraTurnSpeed);
+        }
+
         break;
 
       case backward && leftward:
-        // Handle simultaneous press of "right" and "backward" buttons (BL)
-        newVelocity.x =
-          (movementDirection.x - movementDirection.z) * speed * delta;
-        newVelocity.z =
-          (movementDirection.z + movementDirection.x) * speed * delta;
-        body.current.rotation.y =
-          Math.atan2(
-            state.camera.position.x - body.current.position.x,
-            state.camera.position.z - body.current.position.z
-          ) +
-          Math.PI / 2;
-        setAnimationName(shift ? "Run" : "Walk");
+        {
+          // Handle simultaneous press of "right" and "backward" buttons (BL)
+          newVelocity.x =
+            (movementDirection.x - movementDirection.z) * speed * delta;
+          newVelocity.z =
+            (movementDirection.z + movementDirection.x) * speed * delta;
+
+          // Calc target rotation for backward && leftward
+          const targetRotation = calculateTargetRotation(
+            state.camera.position,
+            body.current.position,
+            Math.PI / 2
+          );
+
+          // Interpolate current with target
+          const smoothedRotation = smoothRotation(
+            body.current.rotation.y,
+            targetRotation,
+            rotationSpeed,
+            delta
+          );
+
+          // Keep the rotation within the range of 0 and 2π
+          body.current.rotation.y =
+            (2 * Math.PI + smoothedRotation) % (2 * Math.PI);
+
+          // Set animation name based on movement speed
+          setAnimationName(shift ? "Run" : "Walk");
+
+          // Apply left camera rotation using cameraTurnSpeed [+ for left]
+          setRotationAngle(rotationAngle + cameraTurnSpeed);
+        }
         break;
 
       case backward && rightward:
-        // Handle simultaneous press of "left" and "backward" buttons (BR)
-        newVelocity.x =
-          (-movementDirection.x - movementDirection.z) * -speed * delta;
-        newVelocity.z =
-          (-movementDirection.z + movementDirection.x) * -speed * delta;
-        body.current.rotation.y =
-          Math.atan2(
-            state.camera.position.x - body.current.position.x,
-            state.camera.position.z - body.current.position.z
-          ) + Math.PI;
-        setAnimationName(shift ? "Run" : "Walk");
+        {
+          // Handle simultaneous press of "left" and "backward" buttons (BR)
+          newVelocity.x =
+            (-movementDirection.x - movementDirection.z) * -speed * delta;
+          newVelocity.z =
+            (-movementDirection.z + movementDirection.x) * -speed * delta;
+
+          // Calc target rotation for backward && rightward
+          const targetRotation = calculateTargetRotation(
+            state.camera.position,
+            body.current.position,
+            Math.PI
+          );
+
+          // Interpolate current with target
+          const smoothedRotation = smoothRotation(
+            body.current.rotation.y,
+            targetRotation,
+            rotationSpeed,
+            delta
+          );
+
+          // Keep the rotation within the range of 0 and 2π
+          body.current.rotation.y =
+            (2 * Math.PI + smoothedRotation) % (2 * Math.PI);
+
+          // Set animation name based on movement speed
+          setAnimationName(shift ? "Run" : "Walk");
+
+          // Apply right camera rotation using cameraTurnSpeed [- for right]
+          setRotationAngle(rotationAngle - cameraTurnSpeed);
+        }
         break;
 
       default:
@@ -187,22 +415,10 @@ export default function TestingMov() {
     body.current.position.y += newVelocity.y;
     body.current.position.z += newVelocity.z;
 
-    // Update the velocity state
-    setVelocity(newVelocity);
-
     /**
      * Camera
      */
     const bodyPosition = body.current.position;
-
-    // Set amount of rotation on target needed on L or R movement
-    if (leftward) {
-      setRotationAngle(rotationAngle + 0.01); // Increase rotation angle
-    }
-
-    if (rightward) {
-      setRotationAngle(rotationAngle - 0.01); // Decrease rotation angle
-    }
 
     // Update camera
     const cameraRotation = THREE.MathUtils.lerp(
@@ -211,12 +427,14 @@ export default function TestingMov() {
       10 * delta
     );
 
+    // Calculate cameras position relative to the body
     const cameraPosition = new THREE.Vector3();
     cameraPosition.copy(bodyPosition);
     cameraPosition.x += Math.sin(cameraRotation) * cameraDistance;
     cameraPosition.z += Math.cos(cameraRotation) * cameraDistance;
     cameraPosition.y += 4; // Adjust the camera's vertical position if needed
 
+    // Position target slightly above character's body
     const cameraTarget = new THREE.Vector3();
     cameraTarget.copy(bodyPosition);
     cameraTarget.y += 0.25;
