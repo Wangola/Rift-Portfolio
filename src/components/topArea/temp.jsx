2698e6cc2fd3630fe1e03d3dc1fd614cd30d26e7
Wangola <wangola99@gmail.com>
2023-08-13 20:23:29 -0400

    Updated banner transitions with useFrame opacity

--- src/components/topArea/temp.jsx
+++ src/components/topArea/temp.jsx
@@ -1,14 +1,34 @@
 import { Html, useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import * as THREE from "three";
 
 function LoadPole({ nodes }) {
-  const [activeMesh, setActiveMesh] = useState("banner");
+  const [activeMesh, setActiveMesh] = useState(false);
 
-  const handleMeshChange = (meshName) => {
-    setActiveMesh(meshName);
+  const handleMeshChange = (meshState) => {
+    setActiveMesh(meshState);
   };
+
+  const bannerRef = useRef();
+  const opacityChangeSpeed = 6;
+
+  useFrame((state, delta) => {
+    /**
+     * ----- Banner transition -----
+     */
+    const targetContactOpacity = activeMesh ? 1 : 0;
+    const currentContactOpacity = bannerRef.current.material.opacity;
+
+    // Animate the banner fade in and out
+    const newOpacity = THREE.MathUtils.lerp(
+      currentContactOpacity,
+      targetContactOpacity,
+      opacityChangeSpeed * delta
+    );
+
+    bannerRef.current.material.opacity = newOpacity;
+  });
 
   return (
     <>
@@ -16,27 +36,24 @@ function LoadPole({ nodes }) {
         geometry={nodes.bannerPole.geometry}
         position={nodes.bannerPole.position}
       >
+        <meshBasicMaterial color={"#CD7F32"} />
         <Html>
-          <button onClick={() => handleMeshChange("banner")}>
-            Show Banner
-          </button>
-          <button onClick={() => handleMeshChange("bannerExpand")}>
-            Show Expand
-          </button>
+          <button onClick={() => handleMeshChange(false)}>Show Banner</button>
+          <button onClick={() => handleMeshChange(true)}>Show Expand</button>
         </Html>
       </mesh>
 
-      <mesh
-        geometry={nodes.banner.geometry}
-        position={nodes.banner.position}
-        visible={activeMesh === "banner"}
-      ></mesh>
+      <mesh geometry={nodes.banner.geometry} position={nodes.banner.position}>
+        <meshBasicMaterial color={"#FF5733"} />
+      </mesh>
 
       <mesh
+        ref={bannerRef}
         geometry={nodes.bannerExpand.geometry}
         position={nodes.bannerExpand.position}
-        visible={activeMesh === "bannerExpand"}
-      ></mesh>
+      >
+        <meshBasicMaterial color={"#FF5733"} transparent opacity={0} />
+      </mesh>
     </>
   );
 }
